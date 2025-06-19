0b4c6e3326ca0c4736e0a064b84443ce58c66c13
郭逸轩 <mcymzc@qq.com>
2025-06-19 20:55:17 +0800

    feat(entity): 优化 AlistConfig 类并添加版本号
    - 在 AlistConfig 类中添加无参构造函数
    - 添加有参构造函数，并在其中处理 alistBaseUrl 的尾部斜杠
    - 更新项目版本号至 1.0.1

--- src/main/java/com/mavis/entity/AlistConfig.java
+++ src/main/java/com/mavis/entity/AlistConfig.java
@@ -2,11 +2,21 @@ package com.mavis.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 public class AlistConfig {
     private String alistBaseUrl;
     private String alistUsername;
     private String alistPassword;
+
+    public AlistConfig(String alistBaseUrl, String alistUsername, String alistPassword) {
+        if (alistBaseUrl.endsWith("/")){
+            alistBaseUrl = alistBaseUrl.substring(0, alistBaseUrl.length() - 1);
+        }
+        this.alistBaseUrl = alistBaseUrl;
+        this.alistUsername = alistUsername;
+        this.alistPassword = alistPassword;
+    }
 }
--- src/main/java/com/mavis/test/Test.java
+++ src/main/java/com/mavis/test/Test.java
@@ -8,12 +8,10 @@ import java.util.HashMap;
 
 public class Test {
     public static void main(String[] args) {
-        AlistConfig alistConfig = new AlistConfig("http://mavis01.top:15244","admin","gyx1129@");
+        AlistConfig alistConfig = new AlistConfig("http://localhost:5244/","admin","admin");
         ArrayList<HashMap<String, String>> alistAllFilesInfo = AlistUtils.getAlistAllFilesInfo(alistConfig, "/files/DockerImage", "");
         for (HashMap<String, String> map : alistAllFilesInfo) {
             System.out.println(map);
         }
-
-
     }
 }
