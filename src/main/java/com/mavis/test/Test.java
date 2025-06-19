6f6a687c1b0e06fdb50ba050e1259a93b14bb6b7
郭逸轩 <mcymzc@qq.com>
2025-06-19 20:57:37 +0800

    feat(entity): 优化 AlistConfig 类并添加版本号
    - 在 AlistConfig 类中添加无参构造函数
    - 添加有参构造函数，并在其中处理 alistBaseUrl 的尾部斜杠
    - 更新项目版本号至 1.0.1

--- src/main/java/com/mavis/test/Test.java
+++ src/main/java/com/mavis/test/Test.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 
 public class Test {
     public static void main(String[] args) {
-        AlistConfig alistConfig = new AlistConfig("http://localhost:5244/","admin","admin");
+        AlistConfig alistConfig = new AlistConfig("http://localhost:5244","admin","admin");
         ArrayList<HashMap<String, String>> alistAllFilesInfo = AlistUtils.getAlistAllFilesInfo(alistConfig, "/files/DockerImage", "");
         for (HashMap<String, String> map : alistAllFilesInfo) {
             System.out.println(map);
